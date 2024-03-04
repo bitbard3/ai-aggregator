ae6e9ee32276fe7ba5bcf04bf07cbe25d1318a25
bitbard3 <ansharora3839@gmail.com>
2024-03-05 01:37:12 +0530

    Improve ui of navigate page

--- frontend/src/pages/Navigate.jsx
+++ frontend/src/pages/Navigate.jsx
@@ -4,15 +4,18 @@ import { BsRobot } from "react-icons/bs";
 import { projects } from "@/lib/projects";
 export default function Navigate() {
   return (
-    <div className=" flex items-center justify-center flex-col min-h-screen min-w-screen bg-dark">
-      <div className="flex items-center gap-3">
-        <BsRobot className="text-light text-3xl"></BsRobot>
-        <h2 className="text-light text-3xl  tracking-wide">
-          Choose your model
-        </h2>
-      </div>
-      <div className="max-w-5xl mx-auto px-8">
-        <HoverEffect items={projects} />
+    <div className="min-h-screen w-full bg-black  bg-grid-white/[0.2]  relative flex items-center justify-center">
+      <div className="absolute pointer-events-none inset-0 flex items-center justify-center bg-black  [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
+      <div className=" flex items-center justify-center flex-col min-h-screen min-w-screen z-10">
+        <div className="flex items-center gap-3">
+          <BsRobot className="text-light text-3xl"></BsRobot>
+          <h2 className="text-light text-3xl  tracking-wide">
+            Choose your model
+          </h2>
+        </div>
+        <div className="max-w-5xl mx-auto px-8">
+          <HoverEffect items={projects} />
+        </div>
       </div>
     </div>
   );
