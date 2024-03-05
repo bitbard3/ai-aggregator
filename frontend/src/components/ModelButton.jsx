923e8430c85d00b34efb4eb581d45c86a6994008
bitbard3 <ansharora3839@gmail.com>
2024-03-05 17:59:23 +0530

    Add model button component

---
+++ frontend/src/components/ModelButton.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function ModelButton({ text }) {
+  return (
+    <button className="px-5 py-1.5 ml-auto mt-4 text-light hover:opacity-90 text-sm bg-neutral-900 border border-neutral-700 rounded-md ">
+      {text}
+    </button>
+  );
+}
