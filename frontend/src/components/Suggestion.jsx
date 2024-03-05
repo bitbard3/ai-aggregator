9416e91a4dd126a1ebdef59f67a333abcb741e33
bitbard3 <ansharora3839@gmail.com>
2024-03-05 17:16:19 +0530

    Add suggestion component

---
+++ frontend/src/components/Suggestion.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { PiLightbulb } from "react-icons/pi";
+import { prompts } from "@/lib/prompts/tts.prompts";
+import SuggestionButton from "./SuggestionButton";
+
+export default function Suggestion() {
+  return (
+    <div className="w-full grid grid-cols-12 gap-4 mt-10">
+      <div className="col-span-12 flex items-center justify-center mb-3">
+        <PiLightbulb className="h-5 w-6 mt-0.5 mr-2 text-light" />
+        <p className="text-light text-lg">Try a sample prompt</p>
+      </div>
+      {prompts.map((prompt) => (
+        <SuggestionButton text={prompt.desc} />
+      ))}
+    </div>
+  );
+}
