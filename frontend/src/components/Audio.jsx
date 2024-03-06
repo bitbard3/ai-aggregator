1bb7c72d89d97c63a1d084c635c6973c27f91d7c
bitbard3 <ansharora3839@gmail.com>
2024-03-06 11:37:41 +0530

    Add functionality to download the output

--- frontend/src/components/Audio.jsx
+++ frontend/src/components/Audio.jsx
@@ -4,12 +4,25 @@ import { useRecoilValue } from "recoil";
 
 export default function Audio() {
   const audioBlob = useRecoilValue(output);
+
+  const handleDownload = () => {
+    const link = document.createElement("a");
+    link.href = audioBlob;
+    link.download = "audio_file.mp3";
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  };
+
   return (
     audioBlob && (
       <>
         <p className="text-light md:text-xl text-lg">Output</p>
         <audio controls src={audioBlob}></audio>
-        <button className="px-5 py-1.5 text-light hover:opacity-90 text-sm bg-neutral-900 border border-neutral-700 rounded-md ">
+        <button
+          onClick={handleDownload}
+          className="px-5 py-1.5 text-light hover:opacity-90 text-sm bg-neutral-900 border border-neutral-700 rounded-md"
+        >
           Download
         </button>
       </>
