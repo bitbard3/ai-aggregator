33b9c37f83b3888fe2db7d9f9605703e059692b8
bitbard3 <ansharora3839@gmail.com>
2024-03-07 14:53:56 +0530

    Add completion model suggested prompts

---
+++ frontend/src/lib/prompts/tttc.prompts.js
@@ -0,0 +1,20 @@
+export const prompts =
+    [
+        {
+            desc: 'I was absent because',
+            text: "Email",
+        },
+        {
+            desc: 'Story',
+            text: "Once upon a time,",
+        },
+        {
+            desc: 'Example 3',
+            text: "My name is Clara and I am",
+        },
+        {
+            desc: 'Story 2',
+            text: "something happended in that scary old house few years back,there lived a girl",
+        },
+
+    ]
