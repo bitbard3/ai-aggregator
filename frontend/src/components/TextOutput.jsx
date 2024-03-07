902e393cf7e4d0a56bc6431cf04eb48327faeb06
bitbard3 <ansharora3839@gmail.com>
2024-03-08 01:21:58 +0530

    Add functionality to continue output as prompt also change copy button

--- frontend/src/components/TextOutput.jsx
+++ frontend/src/components/TextOutput.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { Textarea } from "@/components/ui/textarea";
-import { BsClipboard } from "react-icons/bs";
-import { useRecoilValue } from "recoil";
-import DownloadButton from "./DownloadButton";
+import { useRecoilValue, useSetRecoilState } from "recoil";
+import ActionButton from "./ActionButton";
 import { output } from "@/stores/atoms/output";
 import Loader from "./Loader";
+import { prompt } from "@/stores/atoms/prompt";
 
-export default function TextOutput({ loading }) {
+export default function TextOutput({ loading, complete }) {
   const textOutput = useRecoilValue(output);
+  const setPrompt = useSetRecoilState(prompt);
   const handleDownload = () => {
     if (textOutput) {
       const link = document.createElement("a");
@@ -23,6 +24,9 @@ export default function TextOutput({ loading }) {
   const handleCopy = async () => {
     await navigator.clipboard.writeText(textOutput);
   };
+  const handleContinue = () => {
+    setPrompt(textOutput);
+  };
   return (
     <>
       <Textarea
@@ -31,17 +35,26 @@ export default function TextOutput({ loading }) {
         placeholder="Your output will be shown here..."
         value={textOutput}
       ></Textarea>
-      <div className=" w-full flex justify-end">
-        <DownloadButton
+      <div className=" w-full flex justify-between">
+        <div className="flex items-center md:gap-5 gap-2">
+          <ActionButton
+            onClick={handleCopy}
+            className="mt-5 mb-5"
+            text={"Copy"}
+          />
+          {complete && textOutput && (
+            <ActionButton
+              onClick={handleContinue}
+              className="mt-5 mb-5"
+              text={"Continue"}
+            />
+          )}
+        </div>
+        <ActionButton
           onClick={handleDownload}
           className="mt-5 mb-5"
           text={"Download"}
         />
-      </div>
-      <div className="absolute bottom-3 left-3">
-        <button onClick={handleCopy}>
-          <BsClipboard className="text-neutral-400 h-5 w-5" />
-        </button>
       </div>
       {loading && (
         <div className="h-full z-10 w-full bg-neutral-800 bg-opacity-70 flex items-center justify-center absolute top-0 bottom-0">
