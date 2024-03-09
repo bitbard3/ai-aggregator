201697d746ae3bf496b43d161075b520c7332460
bitbard3 <ansharora3839@gmail.com>
2024-03-09 13:39:28 +0530

    Add user login route

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,11 +1,13 @@
 import { Router } from "express";
-import { signup } from "../controllers/user.contollers.js";
+import { login, signup } from "../controllers/user.contollers.js";
 import { userExist } from "../middlewares/userExist.middleware.js";
 import { validReq } from "../middlewares/validUserInput.middleware.js";
+import { userLogin } from "../middlewares/userLogin.middleware.js";
 
 const router = Router()
 
 router.post('/signup', validReq, userExist, signup)
+router.post('/login', validReq, userLogin, login)
 
 
 export default router
