71872cc497bdefaae3229da224f2e6cf1d3cca36
bitbard3 <ansharora3839@gmail.com>
2024-03-09 13:36:36 +0530

    Add jwt authentication in signup

--- backend/controllers/user.contollers.js
+++ backend/controllers/user.contollers.js
@@ -1,11 +1,16 @@
+import jwt from 'jsonwebtoken'
 import { User } from "../db/db.js"
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 export const signup = async (req, res) => {
     try {
-        await User.create({ email: req.email, password: req.password })
-        return res.json({ msg: "User created" })
+        const user = await User.create({ email: req.email, password: req.password })
+        const token = jwt.sign({ email: req.email, userId: user._id }, process.env.JWT_SECRET)
+        return res.json({ msg: "User created", token })
     } catch (error) {
         res.status(500).json({ error: "Internal Server Error" })
     }
-
 }
+
