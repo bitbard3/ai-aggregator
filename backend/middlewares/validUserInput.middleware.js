2b3bb42c523369db46bc5ac89a4d897b9bf6c6f5
bitbard3 <ansharora3839@gmail.com>
2024-03-12 17:20:13 +0530

    Chnage validUserinput middleware for google sign in

--- backend/middlewares/validUserInput.middleware.js
+++ backend/middlewares/validUserInput.middleware.js
@@ -5,7 +5,8 @@ export const validReq = (req, res, next) => {
     if (!valid.success) {
         return res.status(411).json({ msg: "Invalid inputs" })
     }
-    req.email = req.body.email
-    req.password = req.body.password
+    req.email = req.body.email ? req.body.email : null;
+    req.password = req.body.password ? req.body.password : null;
+    req.googleJwt = req.body.googleJwt ? req.body.googleJwt : null
     next()
 }
