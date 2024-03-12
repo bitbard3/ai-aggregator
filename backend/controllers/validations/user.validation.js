a083cc01cbb68a315291b13470fce460eceded12
bitbard3 <ansharora3839@gmail.com>
2024-03-12 17:19:36 +0530

    Change validation schema

--- backend/controllers/validations/user.validation.js
+++ backend/controllers/validations/user.validation.js
@@ -1,6 +1,8 @@
 import z from 'zod'
 
 export const userSchema = z.object({
-    email: z.string().email().trim().toLowerCase(),
-    password: z.string().min(4)
+    email: z.string().email().trim().toLowerCase().optional(),
+    password: z.string().min(4).optional(),
+    googleSubject: z.string().optional(),
+    googleJwt: z.string().optional()
 })
