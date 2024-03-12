2d8707ea7cf2e55c7a7a88b98c69b1f69ec1148e
bitbard3 <ansharora3839@gmail.com>
2024-03-12 17:21:41 +0530

    Chnage signin controller,for incorporating google sign ins

--- backend/controllers/user.contollers.js
+++ backend/controllers/user.contollers.js
@@ -6,6 +6,16 @@ dotenv.config()
 
 export const signup = async (req, res) => {
     try {
+        if (req.googleJwt) {
+            const payload = jwt.decode(req.googleJwt)
+            const existingUser = await User.findOne({ email: payload.email })
+            if (existingUser) {
+                return res.status(403).json({ msg: "User already exist" })
+            }
+            const user = await User.create({ email: payload.email, googleSubject: payload.sub })
+            const token = jwt.sign({ email: payload.email, userId: user._id }, process.env.JWT_SECRET)
+            return res.json({ msg: "User created", token })
+        }
         const user = await User.create({ email: req.email, password: req.password })
         const token = jwt.sign({ email: req.email, userId: user._id }, process.env.JWT_SECRET)
         return res.json({ msg: "User created", token })
