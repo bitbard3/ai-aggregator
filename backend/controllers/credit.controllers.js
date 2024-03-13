1d2775175ca9d292345a985468b170f0783d3fd7
bitbard3 <ansharora3839@gmail.com>
2024-03-14 04:29:23 +0530

    Add functionality to deduct tokens

--- backend/controllers/credit.controllers.js
+++ backend/controllers/credit.controllers.js
@@ -1,4 +1,26 @@
 import { User } from "../db/db.js"
 import dotenv from 'dotenv'
+import jwt from 'jsonwebtoken'
+import { creditSchema } from "./validations/credit.validation.js"
 dotenv.config()
 
+export const generate = async (req, res) => {
+    const validPayLoad = creditSchema.safeParse({ credit: req.body.credits })
+    if (validPayLoad.error) {
+        return res.status(411).json({ msg: "Please provide valid input" })
+    }
+    try {
+        var user = await User.findOne({ _id: req.userId })
+    } catch (error) {
+        return res.status(404).json({ msg: "User doesnt exist" })
+    }
+    if (user.credits < req.body.credits) {
+        return res.status(400).json({ msg: "Insufficient credits" })
+    }
+    try {
+        await User.updateOne({ _id: req.userId }, { $inc: { credits: -req.body.credits } })
+        res.json({ msg: "Credits deducted" })
+    } catch (error) {
+        return res.status(500).json({ msg: "Something went wrong" })
+    }
+}
--- backend/routes/credit.routes.js
+++ backend/routes/credit.routes.js
@@ -1,6 +1,9 @@
 import { Router } from "express";
+import { generate } from "../controllers/credit.controllers.js";
+import { validUser } from "../middlewares/userAuth.middleware.js";
 
 const router = Router()
 
+router.post('/generate', validUser, generate)
 
 export default router
