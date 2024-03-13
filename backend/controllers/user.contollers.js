fcc17de5ac5b59299b5446817f08b3e6da893b55
bitbard3 <ansharora3839@gmail.com>
2024-03-13 17:32:36 +0530

    Add functionality to signup without google

--- backend/controllers/user.contollers.js
+++ backend/controllers/user.contollers.js
@@ -5,20 +5,32 @@ import { OAuth2Client } from 'google-auth-library';
 dotenv.config()
 
 export const signup = async (req, res) => {
-    const client = new OAuth2Client(process.env.CLIENT_ID, process.env.CLIENT_SECRET);
-    try {
-        const googleToken = await client.verifyIdToken({ idToken: req.googleJwt, audience: process.env.CLIENT_ID })
-        const payload = googleToken.getPayload()
-        const existingUser = await User.findOne({ email: payload.email })
-        if (existingUser) {
-            return res.status(403).json({ msg: "User already exist" })
+    if (req.googleJwt) {
+        const client = new OAuth2Client(process.env.CLIENT_ID, process.env.CLIENT_SECRET);
+        try {
+            const googleToken = await client.verifyIdToken({ idToken: req.googleJwt, audience: process.env.CLIENT_ID })
+            const payload = googleToken.getPayload()
+            const existingUser = await User.findOne({ email: payload.email })
+            if (existingUser) {
+                return res.status(403).json({ msg: "User already exist" })
+            }
+            const user = await User.create({ email: payload.email, googleSubject: payload.sub })
+            const token = jwt.sign({ email: payload.email, userId: user._id }, process.env.JWT_SECRET)
+            return res.json({ msg: "User created", token })
         }
-        const user = await User.create({ email: payload.email, googleSubject: payload.sub })
-        const token = jwt.sign({ email: payload.email, userId: user._id }, process.env.JWT_SECRET)
-        return res.json({ msg: "User created", token })
+
+        catch (error) {
+            return res.status(500).json({ msg: "Something went wrong" })
+        }
     }
-    catch (error) {
-        return res.status(500).json({ msg: "Something went wrong" })
+    else {
+        try {
+            const user = await User.create({ email: req.email, password: req.password })
+            const token = jwt.sign({ email: req.email, userId: user._id }, process.env.JWT_SECRET)
+            return res.json({ msg: "User created", token })
+        } catch (error) {
+            res.status(500).json({ error: "Internal Server Error" })
+        }
     }
 }
 export const login = async (req, res) => {
