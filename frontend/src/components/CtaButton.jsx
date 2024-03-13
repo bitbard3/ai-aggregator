d0897e9b1ebc56a6eb8b2a80675ab556c8c53455
bitbard3 <ansharora3839@gmail.com>
2024-03-14 02:30:50 +0530

    Add trigger to pop modal

--- frontend/src/components/CtaButton.jsx
+++ frontend/src/components/CtaButton.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 export default function CtaButton() {
     return (
-        <button className="inline-flex h-12 text-lg animate-shimmer hover:scale-105 items-center justify-center rounded-md border border-neutral-700 bg-[linear-gradient(110deg,#000103,45%,#35283b,55%,#000103)] bg-[length:200%_100%] px-8 font-medium text-light transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
+        <div className="inline-flex h-12 text-lg animate-shimmer hover:scale-105 items-center justify-center rounded-md border border-neutral-700 bg-[linear-gradient(110deg,#000103,45%,#35283b,55%,#000103)] bg-[length:200%_100%] px-8 font-medium text-light transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
             Signup
-        </button>
+        </div>
     )
 }
