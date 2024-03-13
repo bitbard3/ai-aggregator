86185fb89d26838045cac14ce5ee43dfe0a60c9d
bitbard3 <ansharora3839@gmail.com>
2024-03-14 02:50:27 +0530

    Refactored google signin component for reusability across sign-in and login forms

--- frontend/src/components/GoogleSignin.jsx
+++ frontend/src/components/GoogleSignin.jsx
@@ -4,23 +4,36 @@ import axios from 'axios'
 import { useToast } from "@/components/ui/use-toast"
 import { useNavigate } from "react-router-dom";
 
-export default function GoogleSignin() {
+export default function GoogleSignin({ form }) {
     const { toast } = useToast()
     const navigate = useNavigate();
     const onSignupSuccessHandler = async (data) => {
         try {
-            const res = await axios.post('http://localhost:3000/user/signup', { googleJwt: data.credential })
+            const res = await axios.post(`${import.meta.env.VITE_URL}/user/${form}`, { googleJwt: data.credential })
             const token = res.data.token
             localStorage.setItem('token', token)
-            toast({
-                description: 'Signed in successfully',
-                variant: 'success'
-            })
+            if (form == 'Login') {
+                toast({
+                    description: 'Logged in successfully',
+                    variant: 'success'
+                })
+            } else {
+                toast({
+                    description: 'Signed in successfully',
+                    variant: 'success'
+                })
+            }
             navigate('/models')
         } catch (error) {
             if (error.response.status == 403) {
                 toast({
                     description: 'User already exist',
+                    variant: 'destructive'
+                })
+            }
+            if (error.response.status == 404) {
+                toast({
+                    description: "User doesn't exist!",
                     variant: 'destructive'
                 })
             }
