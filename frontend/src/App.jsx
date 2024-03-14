10a35409ce9aea6f7555ce5cab5c674f01520517
bitbard3 <ansharora3839@gmail.com>
2024-03-14 15:34:14 +0530

    Protect all the models routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,18 +7,21 @@ import Summary from "./pages/Summary";
 import Completion from "./pages/Completion";
 import Grammar from "./pages/Grammar";
 import Landing from "./pages/Landing";
+import ProtectedRoutes from "./util/ProtectedRoutes";
 function App() {
   return (
     <>
       <Routes>
-      <Route path="/" element={<Landing />}></Route>
+        <Route path="/" element={<Landing />}></Route>
         <Route path="/models" element={<Navigate />}></Route>
-        <Route path="/voice" element={<Voice />}></Route>
-        <Route path="/beats" element={<Beats />}></Route>
-        <Route path="/image" element={<Images />}></Route>
-        <Route path="/summary" element={<Summary />}></Route>
-        <Route path="/completion" element={<Completion />}></Route>
-        <Route path="/grammar" element={<Grammar />}></Route>
+        <Route element={<ProtectedRoutes />}>
+          <Route path="/voice" element={<Voice />}></Route>
+          <Route path="/beats" element={<Beats />}></Route>
+          <Route path="/image" element={<Images />}></Route>
+          <Route path="/summary" element={<Summary />}></Route>
+          <Route path="/completion" element={<Completion />}></Route>
+          <Route path="/grammar" element={<Grammar />}></Route>
+        </Route>
       </Routes>
     </>
   );
